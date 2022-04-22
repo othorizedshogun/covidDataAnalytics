fcbb59ba6f37130b529bd23eaba088d3f82931dd
ihatetommy <tommypinsel@gmail.com>
2022-04-22 15:38:16 +0100

    fixed issues with etl.py

--- src/etl.py
+++ src/etl.py
@@ -1,39 +1,42 @@
 import pandas as pd
-from psycopg2 import create_engine
+from sqlalchemy import create_engine
 import os
 import glob
+
+
+# Change list items to preferred table names
+datasetNames = [0, "covid_deaths", "covid_vaccinations"]
 
 def load_data(conn, filepath, table_name):
     # open song file
     dataframe = pd.read_csv(filepath)
-    dataframe.to_sql(table_name, con=conn, index=False, if_exists='replace')
+    dataframe.to_sql(table_name, con=conn, index=False, if_exists="replace")
 
 
 def process_data(conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root, '*.csv'))
+        files = glob.glob(os.path.join(root, "*.csv"))
         for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print("{} files found in {}".format(num_files, filepath))
 
-    # Change list items to preferred table names
-    datasetNames = ['covidDeaths', 'covidVaccinations']
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(conn, datafile, datasetNames[i])
-        print('{}/{} files processed.'.format(i, num_files))
+        print("{}/{} files processed.".format(i, num_files))
 
 def main():
     engine = create_engine("postgresql://postgres:superuser@localhost/covidDataAnalytics")
     conn = engine.connect()
+
     
-    process_data(conn, filepath='../datasets', func=load_data)
+    process_data(conn, filepath="../datasets", func=load_data)
 
     conn.close()
 
